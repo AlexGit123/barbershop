56b2fa37b9d42a47d0527efd1db572db5f45c94c
AlexGit123 <70524253+AlexGit123@users.noreply.github.com>
2022-11-07 00:20:33 +0100

    initial commit

---
+++ laravel/resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+
+import {createApp} from 'vue'
+
+import App from '/Users/alexdoerfer/code/barbershop-1/laravel/resources/js/components/App.vue'
+
+createApp(App).mount("#app")
