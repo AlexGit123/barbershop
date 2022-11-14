9f0c41cc05dcc8aa5e17035af3eed2505621e46d
AlexGit123 <70524253+AlexGit123@users.noreply.github.com>
2022-11-14 21:28:48 +0100

    inital project pages and styling

--- laravel/resources/js/components/App.vue
+++ laravel/resources/js/components/App.vue
@@ -1,4 +1,23 @@
+/*
+* TODO dashboard w name, picture, description
+* Appointments w pictures of barber, calendar, back-end
+* Contact
+* Location
+*/
 <template>
+   <Dashboard></Dashboard>
 </template>
 <script>
+import Dashboard from '/resources/js/components/Dashboard.vue'
+export default {
+    name: 'App',
+    components: {
+        Dashboard
+    }
+}
 </script>
+<style>
+
+</style>
+
+
---
+++ laravel/resources/js/components/Appointment.vue
@@ -0,0 +1,7 @@
+/**
+TODO add API functionality to make it possible to make an appointment
+*/
+<template>
+</template>
+<script>
+</script>
