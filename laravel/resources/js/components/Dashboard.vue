03c5d6a9a792e7a5b298f8dd33ee990faa853743
AlexGit123 <70524253+AlexGit123@users.noreply.github.com>
2023-04-05 16:05:03 +0900

    added tailwind css

--- laravel/resources/js/components/Dashboard.vue
+++ laravel/resources/js/components/Dashboard.vue
@@ -1,6 +1,5 @@
 //TODO Look into interia
 <template>
-
     <body>
         <div class="background">
             <div class="container-fluid">
@@ -13,7 +12,10 @@
                 </div>
                 <div class="text-center pt-5 pb-5">
                     <a>
-                        <button class="btn btn-dark">Make an appointment</button>
+                        <button type="button"
+                            class="inline-block rounded bg-neutral-50 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-neutral-800 shadow-[0_4px_9px_-4px_#fbfbfb] transition duration-150 ease-in-out hover:bg-neutral-100 hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.3),0_4px_18px_0_rgba(251,251,251,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.3),0_4px_18px_0_rgba(251,251,251,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.3),0_4px_18px_0_rgba(251,251,251,0.2)]">
+                            Light
+                        </button>
                     </a>
                 </div>
             </div>
