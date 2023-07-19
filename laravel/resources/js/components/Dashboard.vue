04af2fecce244d4c3db8cb3d67c97d703b232502
AlexGit123 <70524253+AlexGit123@users.noreply.github.com>
2023-07-19 17:49:23 +0200

    added tailwind

--- laravel/resources/js/components/Dashboard.vue
+++ laravel/resources/js/components/Dashboard.vue
@@ -4,7 +4,7 @@
         <div class="background">
             <div class="">
                 <div class="">
-                    <h1>The Barbershop</h1>
+                    <h1 class="text-3xl font-bold underline">Need a cut?</h1>
                 </div>
                 <div class="">
                     <img src="/images/stockshop.jpg" class="">
@@ -18,6 +18,10 @@
                     <br>
                     <h3>Instagram: theshop2022</h3>
                 </div>
+                <!-- <button
+                    class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white">
+                    Button Darker
+                </button> -->
             </div>
         </div>
     </body>
