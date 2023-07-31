03f489bfa2d28879635f5d5c829260c603c3468e
AlexGit123 <70524253+AlexGit123@users.noreply.github.com>
2023-08-01 00:30:59 +0200

    created card style layout and footer area

--- laravel/resources/js/components/Dashboard.vue
+++ laravel/resources/js/components/Dashboard.vue
@@ -1,37 +1,95 @@
-//TODO intertia
+//TODO Set-up Google maps API key
+//TODO Replace card info - map, appointment, make listing
 <template>
     <body>
+
         <div class="background">
-            <div class="">
-                <div class="">
-                    <h1 class="text-3xl font-bold underline">Need a cut?</h1>
-                </div>
-                <div class="flex justify-center">
-                    <div class="mb-6">
-                        <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Search
-                            for a location near you
-                        </label>
-                        <input type="text" id="large-input"
-                            class="block w-half p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <div class="grid grid-cols-2 md:grid-cols-3">
+                <div class="pt-10 pl-10">
+                    <div
+                        class="max-w-sm bg-white border border-gray-500 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div class="p-5">
+                            <svg>
+                                <image href="images/Berlin.svg"></image>
+                            </svg>
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Need a haircut?
+                            </h5>
+                            <label for="large-input"
+                                class="block mb-2 text-sm font-medium text-white-900 dark:text-black ">Search
+                                for a location near you
+                            </label>
+
+                            <div class="flex justify-left">
+                                <input type="text" id="large-input" class="search-bar">
+                            </div>
+                            <div class="flex justify-center pt-10">
+                                <button class="dash-button">Home Page</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <!-- <div class="">
-                    <img src="/images/stockshop.jpg" class="">
-                </div> -->
-                <button class="dash-button">Home Page</button>
-            </div>
-            <!-- Contact -->
-            <div class="">
-                <div class="">
-                    <h1>Contact Us</h1>
-                    <h8>Phone number: +31 6 00000</h8>
-                    <br>
-                    <h3>Instagram: theshop2022</h3>
+
+                <div class="pt-10 pl-10">
+                    <div
+                        class="max-w-sm bg-white border border-gray-500 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div class="p-5">
+                            <svg>
+                                <image href="images/Berlin.svg"></image>
+                            </svg>
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Need a haircut?
+                            </h5>
+                            <label for="large-input"
+                                class="block mb-2 text-sm font-medium text-white-900 dark:text-black ">Search
+                                for a location near you
+                            </label>
+
+                            <div class="flex justify-left">
+                                <input type="text" id="large-input" class="search-bar">
+                            </div>
+                            <div class="flex justify-center pt-10">
+                                <button class="dash-button">Home Page</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <!-- <button
-                    class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white">
-                    Button Darker
-                </button> -->
+
+                <div class="pt-10 pl-10">
+                    <div
+                        class="max-w-sm bg-white border border-gray-500 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div class="p-5">
+                            <svg>
+                                <image href="images/Berlin.svg"></image>
+                            </svg>
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Need a haircut?
+                            </h5>
+                            <label for="large-input"
+                                class="block mb-2 text-sm font-medium text-white-900 dark:text-black ">Search
+                                for a location near you
+                            </label>
+
+                            <div class="flex justify-left">
+                                <input type="text" id="large-input" class="search-bar">
+                            </div>
+                            <div class="flex justify-center pt-10">
+                                <button class="dash-button">Home Page</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--footer-->
+            <div>
+                <div class="flex justify-left">
+                    <div class="mb-6">
+                        <div class="flex justify-left"></div>
+                        <h1 class="left-5 absolute bottom-20">Contact Us</h1>
+                        <div class="absolute bottom-0">
+                            <h3>Phone number: +31 6 00000</h3>
+                            <h3>Instagram: </h3>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </body>
